477a2ad729665d05a9e62d6cc8b18746ba2e2e66
MichalPrzyl <michal.przyl@gmail.com>
2022-01-09 01:50:41 +0100

    better view on cookies

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ const cookiesPerSecondCounter = document.getElementById('cookies-per-second-coun
 const buyClickerBtn = document.getElementById('buy-clicker-btn');
 const buyGrandpaBtn = document.getElementById('buy-grandpa-btn');
 
-var cookies = 10;
+var cookies = 30;
 var cookiesPerSecond = 0;
 
 const buy = (cps, cost) =>{
     if(cookies >= cost){
         cookiesPerSecond += cps;
         cookies -= cost;
-        cookiesCounter.innerHTML=`You have ${cookies} cookies.`
+        cookiesCounter.innerHTML=`You have ${Math.floor(cookies)} cookies.`
         cookiesPerSecondCounter.innerHTML=`Cookies per second: ${cookiesPerSecond}`;
     }
 }
 
 const update = () =>{
-    cookies += cookiesPerSecond;
-    cookiesCounter.innerHTML=`You have ${cookies} cookies.`
+    cookies += cookiesPerSecond/10;
+    cookiesCounter.innerHTML=`You have ${Math.floor(cookies)} cookies.`
 
 }
 
@@ -26,6 +26,6 @@ const startGame=()=>{
     cookiesPerSecondCounter.innerHTML=`Cookies per second: ${cookiesPerSecond}`;
     buyClickerBtn.addEventListener("click", ()=>buy(1,10), false);
     buyGrandpaBtn.addEventListener("click", ()=>buy(2,20), false);
-    setInterval(update, 1000);
+    setInterval(update, 100);
 }
 window.onLoad=startGame();
